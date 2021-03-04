ffa509333e33bf4aaa772d45d062bf8be28238aa
jessiLeandro <jessi_leandro@hotmail.com>
2021-03-04 09:27:40 -0300

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'prettier',
+    'prettier-standard',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['prettier', 'react'],
+  rules: {}
+}
