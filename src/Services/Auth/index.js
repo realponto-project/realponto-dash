77a6d59cf4d8adad0e52c49245c16b70934081b4
jessiLeandro <jessi_leandro@hotmail.com>
2021-03-30 10:11:17 -0300

    change baseUrl

--- src/Services/Auth/index.js
+++ src/Services/Auth/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = `https://alxa-core-staging.herokuapp.com/auth/login`
+const baseUrl = `${process.env.REACT_APP_API_URL}/auth/login`
 
 const auth = async (values) => {
   return await axios.post(baseUrl, values)
