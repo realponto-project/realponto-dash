adb27e02a25bd728fddcd34563dc74392b7690e0
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-04-02 01:45:41 -0300

    hotfix: add get address to customer create

--- src/Containers/Customer/Add/index.js
+++ src/Containers/Customer/Add/index.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { Form, Input, Modal, Row } from 'antd'
 import { map } from 'ramda'
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons'
+import getAddress from '../../../Services/Address'
 
 const rules = [{ required: true, message: 'Este campo é obrigatório!' }]
 
 const customerFormItemsList = [
-  { label: 'Nome', name: 'name', rules },
-  { label: 'Razão Social', name: 'socialName' },
+  { label: 'Nome do cliente', name: 'name', rules },
+  { label: 'Nome Fantasia', name: 'socialName' },
   { label: 'CNPJ/CPF', name: 'document' },
   { label: 'Telefone', name: 'phone' }
 ]
 const addressFormItemsList = [
   { label: 'CEP', name: 'zipcode', rules },
-  { label: 'UF', name: 'states', rules },
+  { label: 'UF', name: 'state', rules },
   { label: 'Cidade', name: 'city', rules },
   { label: 'Bairro', name: 'neighborhood', rules },
   { label: 'Logradouro', name: 'street', rules },
@@ -24,8 +25,13 @@ const addressFormItemsList = [
 
 const renderFormItems = ({ label, name, rules }) => {
   return (
-    <Form.Item key={name} label={label} name={name} rules={rules}>
-      <Input />
+    <Form.Item 
+      key={name}
+      label={label}
+      name={name}
+      rules={rules}  
+    >
+      <Input name={name} />
     </Form.Item>
   )
 }
@@ -38,6 +44,14 @@ const Add = ({
   handleSubmit,
   visible
 }) => {
+  const getCustomerAddress = async ({ target }) => {
+    const { name, value } = target
+    if (name === 'zipcode' && value.length === 8) {
+      const zipcode = await getAddress(value)
+      form.setFieldsValue(zipcode)
+    }
+  }
+
   return (
     <Modal
       onCancel={handleCancel}
@@ -49,7 +63,9 @@ const Add = ({
         labelCol={{ span: 6 }}
         wrapperCol={{ span: 18 }}
         form={form}
-        onFinish={handleSubmit}>
+        onFinish={handleSubmit}
+        onChange={getCustomerAddress}
+      >
         <Form.Item name="id" noStyle />
 
         {map(renderFormItems, customerFormItemsList)}
