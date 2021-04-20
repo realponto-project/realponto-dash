bb90406e9b3737097cc08971cc387534e93afe40
jessiLeandro <jessi_leandro@hotmail.com>
2021-04-20 14:14:50 -0300

    chore: get data for pie

--- src/Containers/Product/Detail/PieChart/index.js
+++ src/Containers/Product/Detail/PieChart/index.js
@@ -1,61 +1,61 @@
 import React from 'react'
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts'
-import { Row, Col, Typography } from 'antd'
+import { Row, Col, Typography, Image } from 'antd'
 import styles from './style.module.css'
+import { isEmpty, map } from 'ramda'
+
+import EmptyImage from '../../../../Assets/empty.svg'
 
 const { Title } = Typography
 const PieChartProduct = ({ pieChartData }) => {
-  const dataChart = [
-    { name: 'Entrada', value: 150 },
-    { name: 'Saída', value: 50 }
-  ]
-
   return (
     <Row gutter={[0, 16]}>
       <Col span={24}>
         <Title className={styles.pieChartTitle}>Movimentações do produto</Title>
       </Col>
-      <Col span={24}>
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart width={800} height={200}>
-            <Tooltip cursor={{ fillOpacity: 0.3 }} />
-            <Pie
-              data={dataChart}
-              innerRadius={70}
-              outerRadius={90}
-              fill="#E6E6E6"
-              paddingAngle={0}
-              dataKey="value">
-              {dataChart.map((item, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={
-                    item.name === 'Entrada'
-                      ? 'rgb(93, 160, 252)'
-                      : 'rgb(23, 201, 178)'
-                  }
-                />
-              ))}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
-      </Col>
-      <Col span={24}>
-        <div className={styles.pieChartLegends}>
-          <span
-            className={styles.spanCircle}
-            style={{ background: 'rgb(93, 160, 252)' }}
-          />
-          <p className={styles.pieChartLegend}>Entrada: {150}</p>
-        </div>
-        <div className={styles.pieChartLegends}>
-          <span
-            className={styles.spanCircle}
-            style={{ background: 'rgb(23, 201, 178)' }}
-          />
-          <p className={styles.pieChartLegend}>Saida: {50}</p>
-        </div>
-      </Col>
+
+      {isEmpty(pieChartData) ? (
+        <Col span={24}>
+          <Image src={EmptyImage} preview={false} alt="Empty" />
+        </Col>
+      ) : (
+        <>
+          <Col span={24}>
+            <ResponsiveContainer width="100%" height={300}>
+              <PieChart width={800} height={200}>
+                <Tooltip cursor={{ fillOpacity: 0.3 }} />
+                <Pie
+                  data={pieChartData}
+                  innerRadius={70}
+                  outerRadius={90}
+                  fill="#E6E6E6"
+                  paddingAngle={0}
+                  dataKey="value">
+                  {pieChartData.map((item, index) => (
+                    <Cell key={`cell-${index}`} fill={item.color} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </Col>
+          <Col span={24}>
+            {map(
+              ({ name, value, color }) => (
+                <div className={styles.pieChartLegends}>
+                  <span
+                    className={styles.spanCircle}
+                    style={{ background: color }}
+                  />
+                  <p className={styles.pieChartLegend}>
+                    {name}: {value}
+                  </p>
+                </div>
+              ),
+              pieChartData
+            )}
+          </Col>
+        </>
+      )}
     </Row>
   )
 }
