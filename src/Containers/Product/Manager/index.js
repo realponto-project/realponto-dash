8704709bb3fc036d5719f050c2f9af76a66771de
jessiLeandro <jessi_leandro@hotmail.com>
2021-07-26 17:37:32 -0300

    fix

--- src/Containers/Product/Manager/index.js
+++ src/Containers/Product/Manager/index.js
@@ -9,7 +9,7 @@ import ProductList from './ProductList'
 
 import { PlusOutlined, SearchOutlined } from '@ant-design/icons'
 import { applySpec, compose, prop, __ } from 'ramda'
-// import { parseValuePTbr } from '../../../utils/Masks/myInfoMasks'
+import { parseValuePTbr } from '../../../utils/Masks/myInfoMasks'
 import { Link } from 'react-router-dom'
 const CheckboxGroup = Checkbox.Group
 
